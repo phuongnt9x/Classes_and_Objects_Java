4d09f54ca4ff937880abce6adbb5f002ffc55328
Nguyen Phuong <95663567+phuongnt9x@users.noreply.github.com>
2022-01-12 14:03:04 +0700

    Comparable_shape

---
+++ system_of_geometric/ComparableCircle.java
@@ -0,0 +1,26 @@
+package system_of_geometric;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+
+	public ComparableCircle() {
+	}
+
+	public ComparableCircle(double radius) {
+		super(radius);
+	}
+
+	public ComparableCircle(double radius, String color, boolean filled) {
+		super(radius, color, filled);
+	}
+
+	@Override
+	public int compareTo(ComparableCircle o) {
+		if (getRadius() > o.getRadius())
+			return 1;
+		else if (getRadius() < o.getRadius())
+			return -1;
+		else
+			return 1;
+	}
+
+}
